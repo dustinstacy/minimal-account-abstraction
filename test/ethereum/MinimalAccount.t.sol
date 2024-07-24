a2fb61dba88c67fe302ef25bc64abff8217c558c
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-24 11:07:39 -0500

    add `MinimalAccount` tests

---
+++ test/ethereum/MinimalAccount.t.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+import {DeployMinimalAccount} from "script/DeployMinimalAccount.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract MinimalAccountTest is Test {
+    HelperConfig helperConfig;
+    MinimalAccount minimalAccount;
+    ERC20Mock usdc;
+
+    address user = makeAddr("user");
+
+    uint256 constant AMOUNT = 1e18;
+
+    function setUp() public {
+        DeployMinimalAccount deployMinimalAccount = new DeployMinimalAccount();
+        (helperConfig, minimalAccount) = deployMinimalAccount.deployMinimalAccount();
+        usdc = new ERC20Mock();
+    }
+
+    function testOwnerCanExecuteCommands() public {
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        vm.prank(minimalAccount.owner());
+        minimalAccount.execute(dest, value, functionData);
+
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
+
+    function test_RevertsIf_NonOwnerAttemptsToExecuteCommands() public {
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        vm.prank(user);
+        vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
+        minimalAccount.execute(dest, value, functionData);
+    }
+}
