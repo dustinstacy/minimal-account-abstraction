fe5a658aa0b27268e60fdd8f184d47533ae5fd1a
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-27 19:23:48 -0500

    add `executeTransaction()`

--- src/zkSync/ZkMinimalAccount.sol
+++ src/zkSync/ZkMinimalAccount.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// zkSync Era imports
 import {
     IAccount,
     ACCOUNT_VALIDATION_SUCCESS_MAGIC
@@ -13,8 +14,12 @@ import {SystemContractsCaller} from
     "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
 import {
     NONCE_HOLDER_SYSTEM_CONTRACT,
-    BOOTLOADER_FORMAL_ADDRESS
+    BOOTLOADER_FORMAL_ADDRESS,
+    DEPLOYER_SYSTEM_CONTRACT
 } from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {Utils} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
+
+// OZ imports
 import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -53,12 +58,22 @@ contract ZkMinimalAccount is IAccount, Ownable {
     /// @notice Thrown when the sender is not the bootloader.
     error ZkMinimalAccount__NotFromBootLoader(address sender);
 
+    /// @notice Thrown when the transaction execution fails.
+    error ZkMinimalAccount__ExecutionFailed();
+
     /*//////////////////////////////////////////////////////////////
                             MODIFIERS
     //////////////////////////////////////////////////////////////*/
 
     modifier requireFromBootLoader() {
         if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
+            revert ZkMinimalAccount__NotFromBootLoader(msg.sender);
+        }
+        _;
+    }
+
+    modifier requireFromBootLoaderOrOwner() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS && msg.sender != owner()) {
             revert ZkMinimalAccount__NotFromBootLoader(msg.sender);
         }
         _;
@@ -110,10 +125,28 @@ contract ZkMinimalAccount is IAccount, Ownable {
         return magic;
     }
 
-    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+    function executeTransaction(bytes32, /*txHash*/ bytes32, /*suggestedSignedHash*/ Transaction memory _transaction)
         external
         payable
-    {}
+        requireFromBootLoader
+    {
+        address to = address(uint160(_transaction.to));
+        uint128 value = Utils.safeCastToU128(_transaction.value);
+        bytes memory data = _transaction.data;
+
+        if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
+            uint32 gas = Utils.safeCastToU32(gasleft());
+            SystemContractsCaller.systemCallWithPropagatedRevert(gas, to, value, data);
+        } else {
+            bool success;
+            assembly {
+                success := call(gas(), to, value, add(data, 0x20), mload(data), 0, 0)
+            }
+            if (!success) {
+                revert ZkMinimalAccount__ExecutionFailed();
+            }
+        }
+    }
 
     function executeTransactionFromOutside(Transaction memory _transaction) external payable {}
 
