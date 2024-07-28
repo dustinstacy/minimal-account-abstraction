d61b456de47bc68c56cea39b78299fd2f47ae1b8
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-27 19:08:32 -0500

    add complete `validateTransaction()`

--- src/zkSync/ZkMinimalAccount.sol
+++ src/zkSync/ZkMinimalAccount.sol
@@ -1,51 +1,113 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
-import {Transaction} from
-    "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {
+    IAccount,
+    ACCOUNT_VALIDATION_SUCCESS_MAGIC
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 import {SystemContractsCaller} from
     "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
-import {NONCE_HOLDER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {
+    NONCE_HOLDER_SYSTEM_CONTRACT,
+    BOOTLOADER_FORMAL_ADDRESS
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
 import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * Lifecycle of a type 113 (0x71) transaction
- * msg.sender is the bootloader system contract
+ * Lifecycle of a type 113 (0x71) transaction.
+ * msg.sender is the bootloader system contract.
  *
  * Phase 1 Validation
- * 1. The user sends the transaction to the "zkSync API client" (sort of a "light node")
- * 2. The zkSync API client checks to see the the nonce is unique by querying the NonceHolder system contract
- * 3. The zkSync API client calls validateTransaction, which MUST update the nonce
- * 4. The zkSync API client checks the nonce is updated
- * 5. The zkSync API client calls payForTransaction, or prepareForPaymaster & validateAndPayForPaymasterTransaction
- * 6. The zkSync API client verifies that the bootloader gets paid
+ * 1. The user sends the transaction to the "zkSync API client" (sort of a "light node").
+ * 2. The zkSync API client checks to see the the nonce is unique by querying the NonceHolder system contract.
+ * 3. The zkSync API client calls validateTransaction, which MUST update the nonce.
+ * 4. The zkSync API client checks the nonce is updated.
+ * 5. The zkSync API client calls payForTransaction, or prepareForPaymaster & validateAndPayForPaymasterTransaction.
+ * 6. The zkSync API client verifies that the bootloader gets paid.
  *
  * Phase 2 Execution
- * 7. The zkSync API client passes the validated transaction to the main node / sequencer (as of today, they are the same)
- * 8. The main node calls executeTransaction
- * 9. If a paymaster was used, the postTransaction is called
+ * 7. The zkSync API client passes the validated transaction to the main node / sequencer (as of today, they are the same).
+ * 8. The main node calls executeTransaction.
+ * 9. If a paymaster was used, the postTransaction is called.
  */
-contract ZkMinimalAccount is IAccount {
+contract ZkMinimalAccount is IAccount, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                           TYPE DECLARATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    using MemoryTransactionHelper for Transaction;
+
+    /*//////////////////////////////////////////////////////////////
+                                ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Thrown when the account does not have enough balance to pay for the transaction.
+    error ZkMinimalAccount__NotEnoughBalance(uint256 required, uint256 actual);
+
+    /// @notice Thrown when the sender is not the bootloader.
+    error ZkMinimalAccount__NotFromBootLoader(address sender);
+
+    /*//////////////////////////////////////////////////////////////
+                            MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    modifier requireFromBootLoader() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
+            revert ZkMinimalAccount__NotFromBootLoader(msg.sender);
+        }
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() Ownable(msg.sender) {}
+
     /*//////////////////////////////////////////////////////////////
                            EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice must increase the nonce
-    /// @notice must validate the transaction (check the owner signed the transaction)
-    /// @notice also check to see if we have enough money in our account
+    /// @notice Must increase the nonce.
+    /// @notice Must check to see if we have enough money in our account.
+    /// @notice Must validate the transaction (check the owner signed the transaction).
     /// @inheritdoc IAccount
-    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+    function validateTransaction(bytes32, /*txHash*/ bytes32, /*suggestedSignedHash*/ Transaction memory _transaction)
         external
         payable
+        requireFromBootLoader
         returns (bytes4 magic)
     {
+        // Increment the nonce.
         SystemContractsCaller.systemCallWithPropagatedRevert(
             uint32(gasleft()),
             address(NONCE_HOLDER_SYSTEM_CONTRACT),
             0,
             abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce))
         );
+
+        // Check to see if we have enough money in our account.
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if (totalRequiredBalance > address(this).balance) {
+            revert ZkMinimalAccount__NotEnoughBalance(totalRequiredBalance, address(this).balance);
+        }
+
+        // Validate the transaction.
+        bytes32 txHash = _transaction.encodeHash();
+        address signer = ECDSA.recover(txHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+        return magic;
     }
 
     function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
